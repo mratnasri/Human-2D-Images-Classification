a3a6bd35e514532d8c87ccd2c59acfa69f85ddb4
mratnasri <mratnasri68@gmail.com>
2021-04-10 20:35:12 +0530

    model19 93.5 accuracy

---
+++ Code/clahe_trial.py
@@ -0,0 +1,14 @@
+import cv2
+import matplotlib as plt
+
+img = cv2.imread('../../Complete 2D Human Images Dataset/1-01.jpg')
+img2 = cv2.imread('../../Complete 2D Human Images Dataset/1-14.jpg')
+gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+gray_img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+clahe = cv2.createCLAHE()
+gray_img = clahe.apply(gray_img)
+gray_img2 = clahe.apply(gray_img2)
+
+cv2.imwrite("../../1.jpg", gray_img)
+cv2.imwrite("../../14.jpg", gray_img2)
+# plt.show()
